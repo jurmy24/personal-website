4ed9c3ab08a1fe0d7ce41c46006d8ff4a896c64f
Victor Oldensand <vmold@kth.se>
2023-07-23 16:39:52 +0200

    Feat: implement a button and alert message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,29 @@
-import Message from './Message'
-
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><Message></Message></div>;
+  let items = ["New York", "San Fransisco", "Stockholm", "London", "Paris"];
+  const [alertVisible, setAlertVisibility] = useState(false);
+
+  const handleSelectItem = (item: string) => console.log(item);
+  return (
+    <>
+      <div>
+        <ListGroup items={items} heading="cities" onSelectItem={handleSelectItem} />
+      </div>
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>You should check in on some of those fields below.</Alert>}
+      <Button
+        onClick={() => {
+          setAlertVisibility(true);
+        }}
+        color="danger"
+      >
+        This is my button
+      </Button>
+    </>
+  );
 }
 
-export default App
+export default App;
