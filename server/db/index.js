36f8aaff009d0967ee6303026ed86ed47867053d
Victor Oldensand <vmold@kth.se>
2023-08-10 19:03:25 +0200

    Chore: remove client and server for eventual restart of project

--- server/db/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/personal-website", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log("out db is connected"))
-.catch((err) => console.log(err)) 
--- server/routers/user.router.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { createUser } = require("../controllers/user.controller");
-
-router.post("/create", createUser);
-
-module.exports = router;
